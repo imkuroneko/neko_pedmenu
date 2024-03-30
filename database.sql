9caf31957528991a312def5dbe98e0e9efc2bebe
KuroNeko <20273059+imkuroneko@users.noreply.github.com>
2024-03-30 01:28:23 -0400

    🛢️ Create database.sql

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `neko_peds` (
+    `license` VARCHAR(64) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `ped_id` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci'
+) COLLATE = 'utf8mb4_general_ci' ENGINE = InnoDB ;
